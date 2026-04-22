aeb325f7a52da2beae3e69b0ed66fbca30bbd231
RichardZhiLi <948945081@qq.com>
2026-04-22 11:19:54 +0800

    feat: 引入heroUI，初始化跟路由界面

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-
+import { Card } from "@heroui/react"
+import './globals.css';
 export default function Home() {
   return (<html>
     <body>
       <div>
-        1111
+        <Card>
+          <Card.Header>
+            <Card.Title>
+              Test
+            </Card.Title>
+            <Card.Content>
+              Test 1111
+            </Card.Content>
+          </Card.Header>
+        </Card>
       </div>
     </body>
   </html>)

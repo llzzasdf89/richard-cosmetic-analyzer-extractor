716a996b0f1e724bc3be2ac6668ffc582a57b99b
RichardZhiLi <948945081@qq.com>
2026-04-24 20:54:53 +0800

    chore: compose配置中增加443端口映射以及容器SSL证书路径映射，修改deploy脚本（因为将服务器迁移到了亚马逊）

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,19 @@
-# deploy.sh
 #!/bin/bash
-#注意需要构建为amd64架构的才行，服务器用的是ubuntu
+
+# 用时间戳或 git commit 作为版本号
+VERSION=$(git rev-parse --short HEAD)
+IMAGE="ccr.ccs.tencentyun.com/richard-lee/richard-cosmetic-analyzer-extractor:$VERSION"
+
+# 构建并推送
 docker buildx build \
   --platform linux/amd64 \
-  -t ccr.ccs.tencentyun.com/richard-lee/richard-cosmetic-analyzer-extractor:v1.0.0 \
+  -t $IMAGE \
   --push \
   . && \
-ssh ubuntu@124.220.221.150 "cd /app && docker compose pull && docker compose up -d"
+
+# 更新服务器上的镜像版本并重启
+ssh -i ./richard.pem ubuntu@13.230.129.167 \
+  "cd ~/app && \
+   sed -i 's|ccr.ccs.tencentyun.com/richard-lee/richard-cosmetic-analyzer-extractor:.*|$IMAGE|' compose.yml && \
+   docker compose pull && \
+   docker compose up -d"

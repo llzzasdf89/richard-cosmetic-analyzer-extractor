4b73a8a390768136dd5f828b71bb5b1548a3a3b6
RichardZhiLi <948945081@qq.com>
2026-04-24 14:53:28 +0800

    fix: 打包回退非standalone模式，因为用standalone模式pina-roll会报错。另外将prompt文件从api/files移动到public目录。docker文件增加日志文件持久化配置

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -7,7 +7,7 @@ import { parseModelJSON, isPDF } from "@/app/lib/utils";
 import { createHandler } from "@/app/lib/api";
 import { rootLogger } from "@/app/lib/logger";
 import type { ChatCompletionMessageParam, FilePurpose } from "openai/resources";
-const PROMPT_CONTENT = fs.readFileSync(path.resolve(process.cwd(), 'app', 'api', 'files', 'prompt.md'))?.toString?.(); //prompt内容
+const PROMPT_CONTENT = fs.readFileSync(path.resolve(process.cwd(), 'public', 'prompt.md'))?.toString?.(); //prompt内容
 const apiKey = 'sk-1fae8b198b114399b097b74f0114586e';
 const openai = new OpenAI({
     apiKey,
